8f1f0d61e4041af59352816b26305ad9f0044fd2
Filip <severuss32167@gmail.com>
2020-12-28 15:00:36 +0100

    Dodaj pliki projektów.

---
+++ Sweeper/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sweeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Map_Size size = new Map_Size();
+
+            int numOfCells = size.Size;
+            int cellSize = 20;
+            Pen p = new Pen(Color.Black);
+
+            for (int y = 0; y < numOfCells; ++y)
+            {
+                g.DrawLine(p, 0, y * cellSize, numOfCells * cellSize, y * cellSize);
+            }
+
+            for (int x = 0; x < numOfCells; ++x)
+            {
+                g.DrawLine(p, x * cellSize, 0, x * cellSize, numOfCells * cellSize);
+            }
+
+        }
+    }
+    class Map_Size
+    {
+         private int size = 64;
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+    }
+
+
+
+
+}
